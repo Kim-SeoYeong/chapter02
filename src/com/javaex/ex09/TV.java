75cb442f66e40fe8e60275d66b82282a92c1bbed
kimsy <duddl2102@naver.com>
2020-11-25 18:07:48 +0900

    TV 문제풀기

--- src/com/javaex/ex09/TV.java
+++ src/com/javaex/ex09/TV.java
@@ -13,6 +13,7 @@ public class TV {
 		this.volume = volume;
 		this.power = power;
 	}
+	
 	//메소드 - getter / setter는 생략
 	public int getChannel() {
 		return channel;
@@ -25,6 +26,7 @@ public class TV {
 	public boolean getPower() {
 		return power;
 	}
+	
 	//메소드 
 	public void power(boolean on) {
 		if (on == true) {
@@ -35,43 +37,54 @@ public class TV {
 	}
 	//1~255 채널 유지(1미만 채널은 1로 바꿔주고 256이상 채널은 255로 변경)
 	public void channel(int channel) {
-		if (channel < 1) {
-			this.channel = 1;
-		} else if (channel > 255) {
-			this.channel = 255;
-		} else {
-			this.channel = channel;
+		if(power == true) {
+			if (channel < 1) {
+				this.channel = 1;
+			} else if (channel > 255) {
+				this.channel = 255;
+			} else {
+				this.channel = channel;
+			}
 		}
 	}
 	//1~255 채널 유지, true일때 1씩 증가, false일때 1씩 감소
 	public void channel(boolean up) {
-		if (up == true) {
-			this.channel++;
-		} else {
-			this.channel--;
+		if(power == true) {
+			if (up == true) {
+				this.channel++;
+			} else {
+				this.channel--;
+			}
 		}
 	}
 	//0~100 볼륨 유지(0미만 볼륨은 1로 0으로 바꾸고 101이상은 100으로 유지)
 	public void volume(int volume) {
-		if (volume < 0) {
-			this.volume = 0;
-		} else if (volume > 100) {
-			this.volume = 100;
-		} else {
-			this.volume = volume;
+		if(power == true) {
+			if (volume < 0) {
+				this.volume = 0;
+			} else if (volume > 100) {
+				this.volume = 100;
+			} else {
+				this.volume = volume;
+			}
 		}
 	}
 	//0~100 볼륨 유지, true일때 1씩 증가, false일때 1씩 감소
 	public void volume(boolean up) {
-		if (up == true) {
-			this.volume++;
-		} else {
-			this.volume--;
+		if(power == true) {
+			if (up == true) {
+				this.volume++;
+			} else {
+				this.volume--;
+			}
 		}
 	}
 	//결과출력
 	public void status() {
-		System.out.println("채널 : " + channel + "번, 볼륨 : " + volume + " , 전원 : " + power);
+		if (power == true) {
+			System.out.println("채널 : " + channel + "번, 볼륨 : " + volume + " , 전원 : " + power);
+		} else {
+			System.out.println("TV의 전원이 꺼져있습니다.");
+		}
 	}
-	
 }
--- src/com/javaex/ex09/TVApp.java
+++ src/com/javaex/ex09/TVApp.java
@@ -9,14 +9,14 @@ public class TVApp {
 		tv.status();	//(채널: 7, 볼륨: 20, 전원: false)
 		
 		tv.power(true);		//(채널: 7, 볼륨: 20, 전원: true)
-		tv.volume(120);		//(채널: 7, 볼륨: 100, 전원: false)
-		tv.status();		//(채널: 7, 볼륨: 100, 전원: false)
+		tv.volume(120);		//(채널: 7, 볼륨: 100, 전원: true)
+		tv.status();		//(채널: 7, 볼륨: 100, 전원: true)
 		
-		tv.volume(false);	//(채널: 7, 볼륨: 99, 전원: false)
-		tv.status();		//(채널: 7, 볼륨: 99, 전원: false)
+		tv.volume(false);	//(채널: 7, 볼륨: 99, 전원: true)
+		tv.status();		//(채널: 7, 볼륨: 99, 전원: true)
 		
-		tv.channel(0);		//(채널: 1, 볼륨: 99, 전원: false)
-		tv.status();		//(채널: 1, 볼륨: 99, 전원: false)
+		tv.channel(0);		//(채널: 1, 볼륨: 99, 전원: true)
+		tv.status();		//(채널: 1, 볼륨: 99, 전원: true)
 		
 		tv.channel(true);	//(채널: 2, 볼륨: 99, 전원: true)
 		tv.channel(true);	//(채널: 3, 볼륨: 99, 전원: true)
@@ -24,7 +24,7 @@ public class TVApp {
 		tv.status();		//(채널: 4, 볼륨: 99, 전원: true)
 		
 		tv.power(false);	//(채널: 4, 볼륨: 99, 전원: flase)
-		tv.status();
+		tv.status();		//TV의 전원이 꺼졌습니다.
 	}
 
 }
